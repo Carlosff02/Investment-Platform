49a8ddfbd6e6fdc7b3beb722ab2d1e0a0bb3fd48
Carlosff02 <carlosfelipefdezfalcon@gmail.com>
2024-01-22 17:16:57 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+          <div className="center">
+            <h1>Conectamos inversionistas con oportunidades de negocio</h1>
+            <h2>Venta de Negocios o Empresas, Franquicias y Emprendimientos</h2>
+          </div>
+          <h1>Conectamos inversionistas con oportunidades de negocio</h1>
+          <h2>Venta de Negocios o Empresas, Franquicias y Emprendimientos</h2>
+  
+          <p>Pulsa sobre la bandera para acceder a la pagina de tu país</p>
+          <a></a>
+        </section>
+        
+    </>
+  );
+}
+
+export default App;
